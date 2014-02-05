6e7d25fbe100008d7c9e2c83c61248cca546f30e
Cyril Bouthors <cyril.bouthors@isvtec.com>
2014-02-05 19:28:22 +0100

    * debian/cron.d: run script every 10s instead of every minute

--- debian/cron.d
+++ debian/cron.d
@@ -1,1 +1,6 @@
 * * * * * root ganeti-failover-gateway-node
+* * * * * root sleep 10; ganeti-failover-gateway-node
+* * * * * root sleep 20; ganeti-failover-gateway-node
+* * * * * root sleep 30; ganeti-failover-gateway-node
+* * * * * root sleep 40; ganeti-failover-gateway-node
+* * * * * root sleep 50; ganeti-failover-gateway-node
